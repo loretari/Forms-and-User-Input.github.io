ee3f230b0622d796a5c33f1905293a9f973e2107
loretari <loretari@gmail.com>
2023-04-18 14:15:37 +0300

    init: initial first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SimpleInput from '../src/components/SimpleInput'
+
+function App() {
+    return (
+        <div className="app">
+<SimpleInput/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/BasicForm.js
@@ -0,0 +1,25 @@
+const BasicForm = (props) => {
+    return (
+        <form>
+<div>
+    <div>
+        <label>First Name</label>
+        <input/>
+    </div>
+    <div>
+        <label></label>
+        <input/>
+    </div>
+</div>
+            <div>
+                <label>E-Mail Address</label>
+                <input/>
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+        </form>
+    )
+}
+
+export default BasicForm;
